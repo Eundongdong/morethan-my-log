00276a12ff50ea9fd880d154d55335fd7aebf55f
Kim Eunji <51512016+Eundongdong@users.noreply.github.com>
2024-06-05 17:17:24 +0900

    Update site.config.js

--- site.config.js
+++ site.config.js
@@ -1,18 +1,18 @@
 const CONFIG = {
   // profile setting (required)
   profile: {
-    name: "Eundongdong",
+    name: "eundongdong",
     image: "/avatar.svg", // If you want to create your own notion avatar, check out https://notion-avatar.vercel.app
     role: "backend developer",
     bio: "I develop the world.",
     email: "kej4373@gmail.com",
     linkedin: "eundongdong",
-    github: "Eundongdong",
+    github: "eundongdong",
     instagram: "",
   },
   projects: [
     {
-      name: `Eundongdong-log`,
+      name: `morethan-my-log`,
       href: "https://github.com/Eundongdong/morethan-my-log",
     },
   ],
@@ -23,7 +23,7 @@ const CONFIG = {
   },
 
   // CONFIG configration (required)
-  link: "https://morethan-log.vercel.app",
+  link: "https://morethan-my-log.vercel.app",
   since: 2022, // If leave this empty, current year will be used.
   lang: "en-US", // ['en-US', 'zh-CN', 'zh-HK', 'zh-TW', 'ja-JP', 'es-ES', 'ko-KR']
   ogImageGenerateURL: "https://og-image-korean.vercel.app", // The link to generate OG image, don't end with a slash
